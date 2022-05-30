592ada8c21d3030d09b65e537a3ade9a3fc141ab
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-05-30 15:47:29 -0400

    calculator fully functional

--- main.js
+++ main.js
@@ -1,41 +1,135 @@
-// reference to display dom element
-let display = document.querySelector("#display");
+//display, first operand, operator, and second operand check
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
 
-// references for the buttons
-let buttons = document.querySelectorAll('.button')
-let clear = document.querySelector("#C")
-let equals = document.querySelector("#equals")
-let decimal = document.querySelector("#decimal")
 
-let result = ''
-// event listener to display button in calcualtor display and decimal functionality
-for (let button of buttons) {
-    button.addEventListener("click", function (event) {
-        console.log(event.target.textContent);
-        let newElement = document.createElement('p');
-        newElement.innerText = event.target.textContent;
-            display.appendChild(newElement);
-    })
-}
+//updating display with input digits from user
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+    //is display currently 0? yes - overwrite with clicked digits; otherwise digit appends using string concatenation
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+    console.log(calculator);
+};
 
-// decimal functionality - NEEDS WORK
-    // if (button === decimal && display.innerText === "") {
-    //     display.textContent = "0.";
-    //     console.log("0.");
-    // } else if (button === decimal && display != "") { 
-    //     display.appendChild(newElement);
-    // } else {
-    //     display.appendChild(newElement);
 
-// clear button functionality
-clear.addEventListener("click", function (event) {
-    console.log(event.currentTarget);
-    display.innerText = "";
+//decimal functionality
+//includes method cheks if decimal point already exists
+function inputDecimal(dot) {
+    //0. if no value entered first
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = "0.";
+        calculator.waitingForSecondOperand === false;
+        return;
+    }
+    // If the `displayValue` does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+      // Append the decimal point
+        calculator.displayValue += dot;
+    }
+};
+
+
+//create first operand/value when operator detected
+function handleOperator(nextOperator) {
+    //destructure calculator object
+    const {firstOperand, displayValue, operator} = calculator
+    //parseFloat converts string to a floating point number
+    const inputValue = parseFloat(displayValue);
+
+    //handle two operators being entered in a row versus value operator value operator
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        console.log(calculator);
+        return;
+    }
+
+    //verify firstOperand is null and that inputValue is not NaN
+    if (firstOperand === null && !isNaN(inputValue)) {
+        //update firstOperand 
+        calculator.firstOperand = inputValue;
+    
+        //handle first value, second value, and operator key being clicked
+    } else if (operator) {
+        const result = calculate(firstOperand, inputValue, operator);
+
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+    console.log(calculator)
+};
+
+
+//second operand/value entered after operator clicked
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === "+") {
+        return firstOperand + secondOperand;
+    } else if (operator === "-") {
+        return firstOperand - secondOperand;
+    } else if (operator === "*") {
+      return firstOperand * secondOperand;
+    } else if (operator === "/") {
+        return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+};
+
+
+//reset calulator with clear button
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    console.log(calculator);
+};
+
+
+// update display
+function updateDisplay () {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+};
+updateDisplay();
+
+
+//detecting which buttons are clicked with event listener
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (!target.matches('button')) {
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        // console.log("operator", target.value);
+        handleOperator(target.value);
+            updateDisplay();
+        return;
+    }
+    if (target.classList.contains("decimal")) {
+        inputDecimal(target.value);
+            updateDisplay();
+        return;
+    }
+    if (target.classList.contains("all-clear")) {
+        resetCalculator();
+            updateDisplay();
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay()
 });
-
-// MATH!!!
-
-// equals button functionality
-
-// other features?
-
--- notesPart2.md
+++ notesPart2.md
@@ -4,7 +4,7 @@ Now that we have captured elements on the page and are able to console log their
 
 [X]First make the values on the buttons appear in the display
 
-[ ]Next make clicking the = sign result in the answer being shown in the display.
+[X]Next make clicking the = sign result in the answer being shown in the display.
 
 Hint: One way of evaluating mathematical expressions is eval. Another way is by using the math.js library.
 
@@ -19,7 +19,7 @@ You should find the above project challenging. However, if you complete it, here
 [ ]Show an error message when invalid input is given. Using try-catch is your best bet here.
 [ ]Disallow invalid input.
 [ ]Allow for chaining long calculations. Order of operations must be honored.
-[ ]Handle edge situations. For example, if a user starts by pressing the decimal button, they generally expect to start a decimal number, which needs a 0 before the decimal point. Add that 0.
+[X]Handle edge situations. For example, if a user starts by pressing the decimal button, they generally expect to start a decimal number, which needs a 0 before the decimal point. Add that 0.
 [ ]Allow entry via the keyboard, not just clicking on buttons.
 [ ]Add a button for calculating the modulo of two numbers.
 [ ]Add a button for raising a number by a power.
